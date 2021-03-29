2faaa265de77d5579738216d7a90742a291e4fb5
Pikachu <wakeupjuaum@gmail.com>
2021-03-28 21:01:09 -0300

    Add OS mensagem Windows

--- API.py
+++ API.py
@@ -1,11 +1,11 @@
-import requests, time, pygame
+import requests, time, pygame, os
 valormaior = 6.50
 pygame.init()
 while True:
     api = requests.get("https://economia.awesomeapi.com.br/all/EUR-BRL").json()
     result = round(float(api['EUR']['ask']), 2)
     if result <= valormaior:
-        print(result)
+        os.system("msg * FICOU MAIS BARATO: {}".format(result))
         pygame.mixer.music.load('C:\\Users\\Joao\\PycharmProjects\\Cotacao_euro\\Music\\teste2.mp3')
         pygame.mixer.music.play()
         time.sleep(20)
