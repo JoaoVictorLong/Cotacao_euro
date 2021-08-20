16c2eef8e3e13189cf435e3b3a5693f6779cc291
pikachudopc <wakeupjuaum@gmail.com>
2021-08-20 19:28:01 -0300

    alteração de importação

--- API.py
+++ API.py
@@ -1,14 +1,30 @@
-import requests, time, pygame, os
-valormaior = 6.50
+from requests import get
+from time import sleep
+from os import system
+import pygame
+import datetime
+count = 0
+valormaior = 5.90
 pygame.init()
+system('cls')
+system("title 'Cotacao do euro'")
 while True:
-    api = requests.get("https://economia.awesomeapi.com.br/all/EUR-BRL").json()
+    try:
+        api = get("http://economia.awesomeapi.com.br/all/EUR-BRL").json()
+    except requests.exceptions.RequestException:
+        print("Erro ao tentar conexão")
+        break
     result = round(float(api['EUR']['ask']), 2)
     if result <= valormaior:
-        os.system("msg * FICOU MAIS BARATO: {}".format(result))
-        pygame.mixer.music.load('C:\\Users\\Joao\\PycharmProjects\\Cotacao_euro\\Music\\teste2.mp3')
-        pygame.mixer.music.play()
-        time.sleep(20)
+        system("msg * FICOU MAIS BARATO: {}".format(result))
+        mixer.music.load('C:\\Users\\joao.victor.longo\\OneDrive - Accenture\\Desktop\\Cotacao_euro\\Music\\teste2.mp3')
+        mixer.music.play()
+        sleep(20)
     else:
-        print("O valor atual é {}".format(result))
-    time.sleep(60)
+        count += 1
+        from datetime import datetime
+        now = datetime.now()
+        frase = "Executando as: {}:{}:{}".format(now.hour,now.minute,now.second)
+        print("O valor atual é {:.2f}".format(result))
+        print(frase, count)
+    sleep(100)
